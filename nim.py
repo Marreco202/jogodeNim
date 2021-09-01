b44812750fffc87b097f27c6da8cb12453b08d01
Guilherme Santos <89481255+MontSantos@users.noreply.github.com>
2021-09-01 17:15:07 -0300

    Código otimizado ;)

--- nim.py
+++ nim.py
@@ -1,71 +1,22 @@
-#JOGO DE NIM
-
-
-def start():
+def main():
     print("=======================")
     print("==== INICIO DO JOGO ====")
     print("=======================\n\n")
-
-    qtd = 12 #quantidade total de bolas no jogo
-    i = 0
-
-    print("QUANTIDADE DE BOLINHAS INICIAIS: ",qtd)
-    
+    qtd= 12
+    i= 0
+    print("\nQTD INICIAL:", qtd)
     while(qtd>0):
-        playerRet = playerTurn(qtd)        
-        qtd-= playerRet
+        jogada= int(input("\nInsira qnts deseja retirar:"))
+        if(jogada>3 or jogada<1 ):
+            print("Número inválido\n")
+        else:
+            nim= 4-jogada
+            print("\nNim tirou:", nim)
+            qtd -= 4
+            print("\ntotal atual:",qtd)
+    print("\nNim ganhou")
+    print("=======================")
+    print("==== FIM DE JOGO ======")
+    print("=======================\n\n")
 
-        nimRet = nimTurn(playerRet,qtd)
-        qtd -=nimRet
-
-        update(playerRet,nimRet,qtd)
-
-        update(playerRet,nimRet,qtd)
-    return
-
-
-
-def nimTurn( pNum,qtd ):
-    ret = 4 - pNum
-    winCheck(qtd- ret,"Nim")
-    return ret
-
-
-
-
-def playerTurn(qtd):
-    num = 4 #valor qualquer para bolas retiradas
-    while(num > 3):
-        num = int(input("Digite a quantidade de bolinhas que deseja retirar: "))
-        if(num >3):
-            print("ERRO: DIGITE UM NUMERO MENOR OU IGUAL A 3\n")
-
-
-
-    winCheck((qtd - num ),"Jogador")
-    return num
-
-
-
-def update(playerRet, nimRet,qtd):
-    print("\nBolinhas retiradas pelo jogador: ",playerRet)
-    print("Bolinhas retiradas pelo Nim: ",nimRet)
-    print("\nBolas restantes: ",qtd)
-    print("\n")
-    
-    return
-
-
-
-
-def winCheck(num,name):
-    if(num <= 0):
-        print(" %s ganhou!"%name)
-        print("=======================")
-        print("==== FIM DE JOGO ======")
-        print("=======================\n\n")
-        exit()
-
-#main
-
-start()
+main()
